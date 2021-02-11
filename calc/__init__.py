ca290dcea356dfeef60ee0500c3bc5731dd510be
gabriel rojas <grojaspiano@gmail.com>
2021-02-10 19:34:02 -0600

    first Commit -GR

---
+++ calc/__init__.py
@@ -0,0 +1,11 @@
+import math
+
+def squareFootage(length,width):
+    area = length * width
+    return area
+
+    
+def cir(r):
+    circumference = 2 * math.pi * r
+    return circumference
+    
